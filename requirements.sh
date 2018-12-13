fd4830963746d31d32cad4b00da128fa67b0cfa0
clsadmin <clsadmin@chs-etq-463-mn003.us-south.ae.appdomain.cloud>
2018-12-13 21:32:05 +0000

    fixed requirements

--- requirements.sh
+++ requirements.sh
@@ -1,9 +1,5 @@
 mkdir /home/wce/clsadmin/data
 
-pip install python-dotenv
-pip install matplotlib
-pip install numpy
-pip install pandas
-pip install os
-pip install time
+pip install --upgrade --user setuptools
+pip install --user python-dotenv
 
